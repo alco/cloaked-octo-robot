39adc1d08b8f179d6c5b4dd9b2893b88a4a8a677
Alexei Sholik <alcosholik@gmail.com>
2012-09-12 23:03:18 +0300

    Add a client

---
+++ client.ex
@@ -0,0 +1,18 @@
+defmodule Client do
+  def connect(address, port) do
+    case :gen_tcp.connect(address, port, [ { :active, false } ]) do
+      { :ok, sock } ->
+        sock
+      other -> other
+    end
+  end
+
+  def close(sock) do
+    :gen_tcp.close(sock)
+  end
+
+  def send(sock, data) do
+    :ok = :gen_tcp.send(sock, data)
+    :gen_tcp.recv(sock, 0)
+  end
+end
