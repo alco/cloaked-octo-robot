f32b4bdcde3c32610dbf6787bb175afc505bd705
Alexei Sholik <alcosholik@gmail.com>
2012-09-20 00:28:39 +0300

    Fix code extractor

--- tools/extract_code.sh
+++ tools/extract_code.sh
@@ -3,5 +3,5 @@
 # This should be run from the root project directory
 
 for file in doc_src/*; do
-    python tools/place_code.py src/echo_server "$file" "${file#doc_src/}"
+    python tools/place_code.py src/echo_server "$file" -o "${file#doc_src/}"
 done
