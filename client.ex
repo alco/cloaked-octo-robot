19e15b994bfc5bd1f313b0e8102d72af4c6ad52d
Alexei Sholik <alcosholik@gmail.com>
2012-09-19 15:14:04 +0300

    Document the client

--- client.ex
+++ client.ex
@@ -1,17 +1,36 @@
 defmodule Client do
+  @moduledoc """
+  A simple TCP client.
+  """
+
+  @doc """
+  Connect to the given address and return a socket suitable for sending data.
+
+    address -- a 4-tuple with IP address components
+    port    -- port number on the remote server
+
+  """
   def connect(address, port) do
-    case :gen_tcp.connect(address, port, [ { :active, false } ]) do
+    case :gen_tcp.connect(address, port, [{:active, false}]) do
       { :ok, sock } ->
         IO.puts "Did connect to server"
         sock
-      other -> other
+
+      { :error, reason } ->
+        IO.puts "Error establishing connection"
     end
   end
 
+  @doc """
+  Close connection.
+  """
   def close(sock) do
     :gen_tcp.close(sock)
   end
 
+  @doc """
+  Send data to the server and wait for a reply.
+  """
   def send(sock, data) do
     :ok = :gen_tcp.send(sock, data)
     :gen_tcp.recv(sock, 0)
