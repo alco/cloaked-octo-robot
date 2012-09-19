f1b8885a63ffb95d73dfcd2d67ed77ae3b516e3b
Alexei Sholik <alcosholik@gmail.com>
2012-09-19 11:51:45 +0300

    Add comments and docstrings to the first server

--- server.ex
+++ server.ex
@@ -1,44 +1,91 @@
 defmodule Server do
+  @moduledoc """
+  This is a basic echo server to demonstrate the usage of Erlang's gen_tcp
+  module.
+
+  It exports one public function start/1 that has an optional `port` argument.
+  """
+
+  @doc """
+  Function for starting the server. Part of the public API.
+  """
+  # When invoked without arguments, `port` will have the value of 8000
   def start(port // 8000) do
+    # :binary is used to get the packets as strings instead of charlists
+    #
+    # { :active, false } creates a passive socket, i.e. we'll need to call
+    # :gen_tcp.recv to get incoming packets
     case :gen_tcp.listen(port, [:binary, { :active, false }]) do
       { :ok, sock } ->
         IO.puts "Listening on port #{port}..."
         accept_loop(sock)
-      other -> other
+
+      { :error, reason } ->
+        IO.puts "Error starting the server: #{reason}"
     end
   end
 
-  def accept_loop(sock) do
+  """
+  This is a private function responsible for spawning new process to handle
+  incoming connections.
+  """
+  defp accept_loop(sock) do
     case :gen_tcp.accept(sock) do
       { :ok, client_sock } ->
-        pid = spawn_client(client_sock)
-
-      case :inet.peername(sock) do
-        { :ok, { address, port } } ->
-          IO.puts "Process #{inspect pid} Got connection from a client: #{inspect address}:#{inspect port}"
-        other ->
-          IO.puts "Process #{inspect pid} Got connection from an unknown client"
-      end
-
+        # Spawn a new process and make a recursive tail-call to continue
+        # accepting new connections.
+        spawn_client(client_sock)
         accept_loop(sock)
-      other -> other
+
+      { :error, reason } ->
+        IO.puts "Failed to accept connections on socket: #{reason}"
     end
   end
 
-  def spawn_client(sock) do
-    spawn __MODULE__, :client_loop, [sock]
+  """
+  Spawn a new process to handle communication over the socket
+  """
+  defp spawn_client(sock) do
+    spawn __MODULE__, :client_start, [sock]
   end
 
-  def client_loop(sock) do
+  @doc false
+  """
+  This function needs to be public in order for `spawn` to be able to invoke it
+  indirectly. By setting @doc to false, we exclude generating the docs for this
+  function indicating that it's not a part of the public API.
+  """
+  def client_start(sock) do
+    pid = Process.self
+
+    # Get info about the client
+    case :inet.peername(sock) do
+      { :ok, { address, port } } ->
+        IO.puts "Process #{inspect pid}: Got connection from a client: #{inspect address}:#{inspect port}"
+
+      { :error, reason } ->
+        IO.puts "Process #{inspect pid}: Got connection from an unknown client (#{reason})"
+    end
+
+    # Start the recieve loop
+    client_loop(sock)
+  end
+
+  """
+  The receive loop which waits for packet from the client and sends it back,
+  effectively turning it into an echo server.
+  """
+  defp client_loop(sock) do
     pid = Process.self
 
     case :gen_tcp.recv(sock, 0) do
       { :ok, packet } ->
-        IO.puts "Process #{inspect pid} got packet #{packet}"
+        IO.puts "Process #{inspect pid}: Got packet #{packet}"
         :gen_tcp.send(sock, packet)
         client_loop(sock)
+
       { :error, reason } ->
-        IO.puts "Process #{inspect pid} did recieve error #{reason}"
+        IO.puts "Process #{inspect pid}: Error receiving a packet: #{reason}"
         :gen_tcp.close(sock)
     end
   end
