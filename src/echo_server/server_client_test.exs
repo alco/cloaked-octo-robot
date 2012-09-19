61143ffcedfefee7a4bb6b6cd56e284dc22b76fa
Alexei Sholik <alcosholik@gmail.com>
2012-09-19 15:25:56 +0300

    Update the test script

--- src/echo_server/server_client_test.exs
+++ src/echo_server/server_client_test.exs
@@ -7,8 +7,8 @@ defmodule ClientTest do
       receive do
       after
         1000 ->
-          { :ok, reply } = Client.send(sock, "Hello, server")
-          IO.puts "Got reply from server: #{reply}"
+          { :ok, reply } = Client.send(sock, "Hello, server #{n}")
+          IO.puts "Process #{inspect Process.self}: Got reply from server: #{reply}"
       end
       send_loop(sock, n - 1)
     else
@@ -20,8 +20,16 @@ end
 # Start the server in a separate process
 spawn Server, :start, []
 
+# Wait for the server process to start
+receive do
+after
+  1000 -> :ok
+end
+
 # Establish connection with the server
 sock = Client.connect({127,0,0,1}, 8000)
 
 # Start sending packets
 ClientTest.send_loop(sock, 10)
+
+IO.puts "All done"
